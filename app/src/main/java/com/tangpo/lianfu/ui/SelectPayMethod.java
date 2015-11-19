83ade5b7ac2d6317b873030463c7ceb4d1c426e7
taiziye <173877860@qq.com>
2015-11-19 18:35:26 +0800

    dd

---
+++ app/src/main/java/com/tangpo/lianfu/ui/SelectPayMethod.java
@@ -0,0 +1,108 @@
+package com.tangpo.lianfu.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.tangpo.lianfu.R;
+import com.tangpo.lianfu.utils.Tools;
+
+public class SelectPayMethod extends FragmentActivity implements View.OnClickListener {
+
+    private TextView pay_by_alipay;
+    private TextView pay_by_wechat;
+    private TextView pay_by_bankcard;
+    private LinearLayout dialogLayout;
+
+    private Bundle bundle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_select_pay_method);
+        Tools.gatherActivity(SelectPayMethod.this);
+        init();
+
+    }
+
+    private void init(){
+        dialogLayout= (LinearLayout) findViewById(R.id.dialog_layout);
+        dialogLayout.setOnClickListener(this);
+
+        pay_by_alipay= (TextView) findViewById(R.id.pay_by_alipay);
+        pay_by_alipay.setOnClickListener(this);
+
+        pay_by_wechat= (TextView) findViewById(R.id.pay_by_wechat);
+        pay_by_wechat.setOnClickListener(this);
+
+        pay_by_bankcard= (TextView) findViewById(R.id.pay_by_bankcard);
+        pay_by_bankcard.setOnClickListener(this);
+
+        bundle=getIntent().getExtras();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_select_pay_method, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            finish();
+            return false;
+        } else {
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.pay_by_alipay:
+                Intent intent=new Intent(SelectPayMethod.this,PayByAliPay.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                break;
+            case R.id.pay_by_wechat:
+
+                break;
+            case R.id.pay_by_bankcard:
+
+                break;
+            default:
+                finish();
+        }
+    }
+}
---
+++ app/src/main/java/com/tangpo/lianfu/utils/Key.java
@@ -0,0 +1,15 @@
+package com.tangpo.lianfu.utils;
+
+/**
+ * Created by shengshoubo on 2015/11/19.
+ */
+public class Key {
+    // 商户PID
+    public static final String PARTNER = "2088021025564436";
+    // 商户收款账号
+    public static final String SELLER = "";
+    // 商户私钥，pkcs8格式
+    public static final String RSA_PRIVATE = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAKkFoH7qwKrqB2rAmTq1mafjh3TOC2eQ35xSGEqUyMvp4LF9XvdKxk8GV0a7mopZZUROjjD1J7m6+AVVQtO+cZsa/jFwd8vNFL6B44tEq3h2B8UUucP+2zO2oi96ACFuMFGCR6iWhYW0RmJPk2qqOOB466P5jM7+TNF8DZFHQZLVAgMBAAECgYBS1/k0e+d3XkHIZyIzjnMDwRfRfnREjNOtEOfrFkXc/IMvO53iY3oTna4Fc0pp6SsjccVr5Z1VAXkzU/jvQ/b2HOMf7MifJF03mdiXX4Bwe/QBY7/A8VAx9yilEFYiAaLTcfMMkABt6iPVeEMgr9y+EFId2JNxfhZvdGqN5duwuQJBANxPCAEPlS4VKr49HyuE8zkdLaRnSrDq7vNJ/mJKuTeD4fbH4bSzwhME5IihrEPv03NfxMJyMqDsRc9Vn6udSIcCQQDEZ45tSGY1MNyQ8iPMEeEXtdaDIyUOnm0ogYU6Xe4KjBIKCWh3NbxXVOiNo1TDKvVLrXReesF1hpB3Mcv0ywzDAkAvSZtrNuALcymoidAN9EyMntG0LyOeE99UQzah/G4DF4DCCqdMtSTcFjnDhghG0MxvQEgogICTgAN//F34ZRszAkEAlaOAtGqQ1L3hjt2twHFu2i3Evi0ssrKp4E6uPwYJsyd9JwB8AzeBw9gBRsjGb9xu/7WsD7/fXbekoevixSNfGQJAP/KOSwVgKsjokcqwz5J7ODjTqnp43c/hkddBek5PXzAsshGdRcawesUZj6kkPO9YtAQ5Siup20XaYMDuHglAaQ==";
+    // 支付宝公钥
+    public static final String RSA_PUBLIC = "";
+}
